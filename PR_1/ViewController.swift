52d8452cc62818045b0c75084cfe23c86eb07bc4
Станислав Зверьков <zverk3333@gmail.com>
2022-01-31 18:44:10 +0300

    Initial Commit

---
+++ PR_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PR_1
+//
+//  Created by Станислав Зверьков on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
